fb7cc301e5bad398bf2a04299c1bb02f1473e020
Salwa <herotony111@gmail.com>
2017-08-15 14:51:45 +0300

    add querydata

---
+++ src/queries/db_functions.js
@@ -0,0 +1,13 @@
+const dbConnection = require('../../database/db_connection.js');
+
+const getData = (cb) => {
+  dbConnection.query('SELECT cohorts.id ,cohorts.location ,cohorts.num , weeks.num ,weeks.title from cohorts inner join cohorts_mentors on cohorts.id = cohort_mentor.cohort_id inner join weeks on weeks.week_num = cohort_mentor.week_num  ', (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res.rows);
+    }
+  });
+};
+
+module.exports = getData;
