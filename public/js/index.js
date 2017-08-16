6a323004f833fe0ff2ed7943a0d299ffb8a9f124
Abdallah <you@example.com>
2017-08-16 14:34:37 +0300

    js files

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,26 @@
+var table = document.querySelector('#weeks-mentor-table');
+get('/weeks?cohort=2', (response) => {
+  var weekNumbers = Object.keys(response);
+  weekNumbers.forEach((week,i) => {
+    var row = table.insertRow(i);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = `week${week}`;
+    cell2.innerHTML = response[week].title;
+    cell3.innerHTML = response[week].mentors.toString();
+
+  });
+})
+
+
+
+
+
+
+
+
+
 // function weekmentorDOM (err, data) {
 //   if (err) {
 //   } else {
@@ -18,7 +41,3 @@
 // }?
 //
 // request('/mentors', weekmentorDOM);
-
-get('/weeks?cohort=2',(response)=>{
-  console.log(response);
-})
