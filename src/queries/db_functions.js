7e0587b830602cdcf7246c4f47bc9d857469bd92
Salwa <herotony111@gmail.com>
2017-08-16 10:29:02 +0300

    modify db_fun

--- src/queries/db_functions.js
+++ src/queries/db_functions.js
@@ -1,6 +1,24 @@
 const dbConnection = require('../database/db_connection.js');
 
 
+const getData = (cohort,cb) => {
+  // var location ='';
+  // var num ='';
+  // var c = cohort.toLowerCase().split('c');
+  // if(c[1]= startsWith('g') ){
+  //   var g = cohort.toLowerCase().split('g');
+  //   return { location : 'Gaza' ,  num : g[1]}
+  // }
+
+  dbConnection.query(`SELECT weeks.num , weeks.week_title , mentors.githubuser from weeks inner join cohort_mentor on weeks.num = cohort_mentor.week_num inner join mentors on mentors.githubuser = cohort_mentor.mentor_user  where  cohort_mentor.cohort_id  = ${cohort}`, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res.rows);
+    }
+  });
+};
+
  const addCohort = (obj, cb) =>{
  	dbConnection.query(`INSERT INTO cohorts (location ,num,start_date) VALUES ('${obj.location}', ${obj.num}, '${obj.start_date}')`, (err, res)=>{
  		if(err){
@@ -20,4 +38,4 @@ const dbConnection = require('../database/db_connection.js');
  		}
  	})
  }
-module.exports = {addCohort, testInsert};
+module.exports = {getData ,addCohort, testInsert};
--- tests/db.test.js
+++ tests/db.test.js
@@ -3,6 +3,23 @@ const shot = require('shot');
 const fs = require('fs');
 const functions = require ('../src/queries/db_functions.js');
 
+
+test('get 16 weeks of selected cohort', (t) => {
+  functions.getData(1,(err,res)=> {
+    if (err){
+      console.log(err);
+    }else{
+    var actual = res;
+    var expected = ['week1','week2','week3','week4','week5','week6','week7','week8','week10','week11','week12'];
+
+    t.deepEqual(actual, expected, 'should return an 16 weeks of selected cohort');
+    t.end();
+  }
+    });
+ });
+
+
+
 test('Add new cohort', (t) => {
 var obj={location:'Gaza',num:2,start_date:'01-01-2010'}
 
